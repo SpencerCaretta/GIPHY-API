8ab50f7ba8ed8d49628a72652f311401c6caab58
Spencer Caretta <spencer.caretta@gmail.com>
2018-10-26 14:34:51 -0500

    initial coding

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+var topics = ["Poker", "Blackjack", "Pokemon", "Yu-Gi-Oh!", "Cardfight Vanguard", "Hearthstone", "Magic The Gathering", "The Elder Scrolls: Legends", "Shadowverse"]
+
+for (i=0; i<topics.length; i++) {
+    $("#buttons").append("<button type='button'></button>")
+}
