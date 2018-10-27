9cebc1aa23066fb0d814ccfa5ed80c7294c95b64
Spencer Caretta <spencer.caretta@gmail.com>
2018-10-27 02:06:31 -0500

    got buttons to show up on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,17 @@
+//our topic array
 var topics = ["Poker", "Blackjack", "Pokemon", "Yu-Gi-Oh!", "Cardfight Vanguard", "Hearthstone", "Magic The Gathering", "The Elder Scrolls: Legends", "Shadowverse"]
 
-for (i=0; i<topics.length; i++) {
-    $("#buttons").append("<button type='button'></button>")
+
+//function used to create all our buttons
+function createButtons() {
+    //Used to make sure there are no duplicate buttons when the user adds buttons later on
+    $("buttons-here").empty();
+    //for loop to add a button for each string in the array
+    for (i=0; i<topics.length; i++) {
+        var abutton = $("<button>");
+        abutton.text(topics[i]);
+        $("#buttons-here").append(abutton);
+    }
 }
+
+createButtons();
