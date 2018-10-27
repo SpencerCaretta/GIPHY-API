7161d1738ea3bf37182fc522bbcb1022f211a32b
Spencer Caretta <spencer.caretta@gmail.com>
2018-10-27 11:31:49 -0500

    change to API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 //our topic array
-var topics = ["Poker", "Blackjack", "Pokemon", "Yu-Gi-Oh!", "Cardfight Vanguard", "Hearthstone", "Magic The Gathering", "The Elder Scrolls: Legends", "Shadowverse"]
+var animals = ["Pig", "Eagle", "Cow", "Chicken", "Duck", "Wombat", "Monkey", "Dog", "Cat"]
 
 
 //function used to create all our buttons
@@ -7,17 +7,19 @@ function createButtons() {
     //Used to make sure there are no duplicate buttons when the user adds buttons later on
     $("buttons-here").empty();
     //for loop to add a button for each string in the array
-    for (i=0; i<topics.length; i++) {
-        var abutton = $("<button>");
-        abutton.text(topics[i]);
+    for (i=0; i<animals.length; i++) {
+        var abutton = $("<button data-animal = 'animals[i]'>");
+        abutton.text(animals[i]);
         $("#buttons-here").append(abutton);
     }
 }
 
 createButtons();
 
-$("buttons-here").on("click", function() {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=g4E3tCjjDQVeGcSI15gFw7acBt0kUX1p&limit=10"
+$("button").on("click", function() {
+    var animals = $(this).attr("data-animal");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animals
+     + "&api_key=g4E3tCjjDQVeGcSI15gFw7acBt0kUX1p&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -25,6 +27,7 @@ $("buttons-here").on("click", function() {
     })
         .then(function(response) {
             var results = response.data;
+            
             for (i=0; i<results.length; i++) {
                 var gifDiv = $("<div class='item'>");
 
@@ -32,11 +35,11 @@ $("buttons-here").on("click", function() {
 
                 var p = $("<p>").text("Rating: " + rating);
 
-                var topicsImage = $("<img>");
-                topicsImage.attr("src", results[i].images.fixed_height.url);
+                var animalsImage = $("<img>");
+                animalsImage.attr("src", results[i].images.fixed_height.url);
 
                 gifDiv.prepend(p);
-                gifDiv.prepend(topicsImage);
+                gifDiv.prepend(animalsImage);
 
                 $("#gifs-here").prepend(gifDiv);
                 }
